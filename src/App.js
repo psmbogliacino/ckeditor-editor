ba89d3aa68811310ce2f50cc5496741c7226ff95
Bogliacino Mauricio <mbogliacino@primerosystems.com>
2017-06-06 15:17:43 -0300

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CKEditor from './CKEditor/CKEditor'
+
 
 class App extends Component {
+
+  onChange (editorState) {
+      console.log(editorState);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Editor</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <CKEditor id="instance123" value="hola mundo" onChange={this.onChange} />
+
+
       </div>
     );
   }
